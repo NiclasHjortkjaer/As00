6c674d5444e0dd9d0179735f910c2bfa375dc552
NiclasHjortkjaer <32992549+NiclasHjortkjaer@users.noreply.github.com>
2021-09-06 13:44:43 +0200

    Feature/boilerplate (#2)
    * Created user interface for IsLeapYear and implemented error handling
    
    Co-authored-by: Niclas Søgaard Hjortkjær <nihj@itu.dk>

--- Project01.Tests/UnitTest1.cs
+++ Project01.Tests/UnitTest1.cs
@@ -1,5 +1,6 @@
 using System;
 using Xunit;
+using System.IO;
 
 namespace Project01.Tests
 {
@@ -36,6 +37,57 @@ namespace Project01.Tests
 
             //Assert
             Assert.True(leapYear);
+        }
+
+        [Fact]
+        public void IsLeapYear1200(){
+            //Act
+            bool leapYear = Program.IsLeapYear(1200);
+
+            //Assert
+            Assert.False(leapYear);
+        }
+
+        [Fact]
+        public void Main_prints_yay(){
+            //Arrange
+            StringWriter writer = new StringWriter();
+            Console.SetOut(writer);
+
+            //Act
+            Program.Main(new String[]{"2020"});
+            var output = writer.GetStringBuilder().ToString().Trim();
+
+            //Assert
+            Assert.Equal("yay", output);
+        }
+
+        [Fact]
+        public void Main_prints_nay(){
+            //Arrange
+            StringWriter writer = new StringWriter();
+            Console.SetOut(writer);
+
+            //Act
+            Program.Main(new String[]{"2021"});
+            var output = writer.GetStringBuilder().ToString().Trim();
+
+            //Assert
+            Assert.Equal("nay", output);
+        }
+
+        [Fact]
+        public void Main_prints_error(){
+            //Arrange
+            StringWriter writer = new StringWriter();
+            Console.SetOut(writer);
+
+            //Act
+            Program.Main(new String[]{"Hello World!"});
+            var output = writer.GetStringBuilder().ToString().Trim();
+
+            //Assert
+            Assert.Equal("Please only input integers", output);
         }
     }
 }
