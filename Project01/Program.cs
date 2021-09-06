4819280b0331417dbc9a54740822c103954a0213
Niclas Søgaard Hjortkjær <nihj@itu.dk>
2021-09-06 13:32:58 +0200

    Created user interface for IsLeapYear and implemented error handling

--- Project01/Program.cs
+++ Project01/Program.cs
@@ -6,11 +6,20 @@ namespace Project01
     {
 
         public static void Main(String[] args){
-
+            string input = args[0];
+            try{
+                int year = int.Parse(input);
+                if(IsLeapYear(year))
+                    Console.WriteLine("yay");
+                else
+                    Console.WriteLine("nay");
+            } catch(FormatException){
+                Console.WriteLine("Please only input integers");
+            }
         }
 
         public static bool IsLeapYear(int year){
-            if(year % 100 == 0 && year % 400 != 0)
+            if(year % 100 == 0 && year % 400 != 0 || year < 1582)
                 return false;
             else if (year % 4 == 0)
                 return true;
