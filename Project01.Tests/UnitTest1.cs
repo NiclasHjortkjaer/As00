cbc5b0c62e3c7f148f6b158539546982f971a4f0
Niclas Søgaard Hjortkjær <nihj@itu.dk>
2021-09-06 12:06:41 +0200

    UnitTest1

--- Project01.Tests/UnitTest1.cs
+++ Project01.Tests/UnitTest1.cs
@@ -6,9 +6,36 @@ namespace Project01.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void IsLeapYear2016()
         {
+            //Arrange
+            
 
+            //Act
+            bool leapYear = Program.IsLeapYear(2016);
+
+            //Assert
+            Assert.True(leapYear);
+        }
+
+        [Fact]
+        public void IsLeapYear2100()
+        {
+            //Act
+            bool leapYear = Program.IsLeapYear(2100);
+
+            //Assert
+            Assert.True(leapYear);
+        }
+
+        [Fact]
+        public void IsLeapYear2400()
+        {
+            //Act
+            bool leapYear = Program.IsLeapYear(2400);
+
+            //Assert
+            Assert.True(leapYear);
         }
     }
 }
